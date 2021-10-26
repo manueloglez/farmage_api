b33551dc2020b3284253bdb96a86ed872d9709a0
manueloglez <mang.95@gmail.com>
2021-10-26 13:27:02 -0700

    added new url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5500', '127.0.0.1:5500', 'localhost:3001', 'https://farmagecli.herokuapp.com'
+    origins 'localhost:5500', '127.0.0.1:5500', 'localhost:3001', 'https://farmagecli.herokuapp.com', 'https://b5b2-24-85-121-206.ngrok.io'
     resource(
       '/api/*', 
       credentials: true,
