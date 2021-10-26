85b4510ffab9edc2b24606bdcddc75b91cb105e3
manueloglez <mang.95@gmail.com>
2021-10-26 13:32:34 -0700

    fixing secure tag

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,7 @@
-Rails.application.config.action_dispatch.cookies_same_site_protection = :none
+Rails.application.config.session_store :cookie_store, {
+  :key => '_application_session',
+  :domain => :all,
+  :same_site => :none,
+  :secure => :true,
+  :tld_length => 2
+}
